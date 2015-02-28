51a8fcdb62ebabd62ca2c9e2f05df08610fcfda8
Lloyd Ramey <lnr0626@gmail.com>
2015-02-28 14:21:58 -0600

    Added AudioTrack creation

--- android/app/src/main/java/tens_bucket/ptens/AudioGeneratorParams.java
+++ android/app/src/main/java/tens_bucket/ptens/AudioGeneratorParams.java
@@ -5,4 +5,28 @@ public class AudioGeneratorParams {
     private volatile int amplitude;
 
     private int sampleRate;
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+
+    public int getAmplitude() {
+        return amplitude;
+    }
+
+    public void setAmplitude(int amplitude) {
+        this.amplitude = amplitude;
+    }
+
+    public int getSampleRate() {
+        return sampleRate;
+    }
+
+    public void setSampleRate(int sampleRate) {
+        this.sampleRate = sampleRate;
+    }
 }
--- android/app/src/main/java/tens_bucket/ptens/AudioGeneratorTask.java
+++ android/app/src/main/java/tens_bucket/ptens/AudioGeneratorTask.java
@@ -1,12 +1,33 @@
 package tens_bucket.ptens;
 
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.os.AsyncTask;
+
+import com.google.common.base.Preconditions;
 
 public class AudioGeneratorTask extends AsyncTask<AudioGeneratorParams, Void, Void> {
 
+    private AudioTrack track;
+    private int minBufferSize;
+
+    private static final int channelConfig = AudioFormat.CHANNEL_OUT_MONO;
+    private static final int audioFormat = AudioFormat.ENCODING_PCM_16BIT;
+
     @Override
     protected Void doInBackground(AudioGeneratorParams... params) {
-        
+        Preconditions.checkNotNull(params);
+        Preconditions.checkArgument(params.length == 1, "AudioGeneratorTask can only handle " +
+                "a single parameter");
+        AudioGeneratorParams parameters = params[0];
+
+        minBufferSize = AudioTrack.getMinBufferSize(parameters.getSampleRate(),
+                channelConfig, audioFormat);
+
+        track = new AudioTrack(AudioManager.STREAM_MUSIC, parameters.getSampleRate(),
+                channelConfig, audioFormat, minBufferSize, AudioTrack.MODE_STREAM);
+
         return null;
     }
 }
