ddde2942c45f4740d0aafa3033aa179a1aad7cb3
Conor Patrick <conorpp@vt.edu>
2015-03-01 04:53:40 -0500

    added graph and toggle buttons

--- android/app/src/main/java/tens_bucket/ptens/SignalGenerator.java
+++ android/app/src/main/java/tens_bucket/ptens/SignalGenerator.java
@@ -1,5 +1,6 @@
 package tens_bucket.ptens;
 
+import android.app.FragmentTransaction;
 import android.media.AudioManager;
 import android.media.AudioTrack;
 import android.support.v4.app.FragmentActivity;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Switch;
 
 import tens_bucket.ptens.fragments.PadFragment;
 import tens_bucket.ptens.signal_generator.SignalGeneratorParameter;
@@ -16,11 +18,14 @@ import tens_bucket.ptens.signal_generator.SignalGeneratorTask;
 public class SignalGenerator extends FragmentActivity {
 
     private static final String LOG_TAG = SignalGenerator.class.getSimpleName();
-    private Button stopButton;
-    private Button startButton;
+
+    private Switch pad1Switch;
+    private Switch pad2Switch;
 
     private SignalGeneratorTask backgroundTask;
     private SignalGeneratorParameter parameters;
+    private PadFragment pad2;
+    private PadFragment pad1;
 
 
     @Override
@@ -28,16 +33,19 @@ public class SignalGenerator extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sine_generator);
 
-        startButton = (Button) findViewById(R.id.start_button);
-        stopButton = (Button) findViewById(R.id.stop_button);
+
+        pad1Switch = (Switch) findViewById(R.id.switch1);
+        pad2Switch = (Switch) findViewById(R.id.switch2);
 
 
         parameters = new SignalGeneratorParameter();
-        PadFragment pad1 =(PadFragment)(getFragmentManager().findFragmentById(R.id.pad_1));
+        pad1 = (PadFragment)(getFragmentManager().findFragmentById(R.id.pad_1));
         pad1.setWaveParameters(parameters.leftWave);
+        parameters.leftWave.setFragment(pad1);
 
-        PadFragment pad2 =(PadFragment)(getFragmentManager().findFragmentById(R.id.pad_2));
+        pad2 = (PadFragment)(getFragmentManager().findFragmentById(R.id.pad_2));
         pad2.setWaveParameters(parameters.rightWave);
+        parameters.rightWave.setFragment(pad2);
 
 
         int sampleRate = AudioTrack.getNativeOutputSampleRate(AudioManager.STREAM_MUSIC);
@@ -56,20 +64,33 @@ public class SignalGenerator extends FragmentActivity {
         }
     }
 
-    public void stop(View view) {
-        changeButtonStates(false);
-        backgroundTask.cancel(true);
+    public void toggle(View view) {
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.setCustomAnimations(android.R.animator.fade_in,
+                android.R.animator.fade_out);
+        if (view == pad1Switch){
+            if (pad1Switch.isChecked()) {
+                ft.show(pad1);
+                parameters.leftWave.setEnabled(true);
+            }else{
+                ft.hide(pad1);
+                parameters.leftWave.setEnabled(false);
+            }
+
+        }
+        else if (view == pad2Switch){
+            if (pad2Switch.isChecked()) {
+                ft.show(pad2);
+                parameters.rightWave.setEnabled(true);
+            }else{
+                ft.hide(pad2);
+                parameters.rightWave.setEnabled(false);
+
+            }
+
+        }
+        ft.commit();
     }
 
-    public void start(View view) {
-        changeButtonStates(true);
-        backgroundTask = new SignalGeneratorTask();
-        backgroundTask.execute(parameters);
 
-    }
-
-    private void changeButtonStates(boolean started) {
-        stopButton.setEnabled(started);
-        startButton.setEnabled(!started);
-    }
 }
--- android/app/src/main/java/tens_bucket/ptens/signal_generator/SignalParameter.java
+++ android/app/src/main/java/tens_bucket/ptens/signal_generator/SignalParameter.java
@@ -1,9 +1,24 @@
 package tens_bucket.ptens.signal_generator;
 
+import android.provider.ContactsContract;
+
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.concurrent.Semaphore;
+
+import tens_bucket.ptens.fragments.PadFragment;
+
 public class SignalParameter {
     private volatile int frequency;
     private volatile double dutyCycle;
     private volatile double amplitude;
+
+    private PadFragment fragment;
+    private volatile boolean enabled = true;
+
+    private LineGraphSeries<DataPoint> dataPlot = new LineGraphSeries<DataPoint>();
+
 
     public SignalParameter() {
         this(1, 4.5, 1.0);
@@ -38,4 +53,58 @@ public class SignalParameter {
     public void setAmplitude(double amplitude) {
         this.amplitude = amplitude;
     }
+
+    public PadFragment getFragment() {
+        return fragment;
+    }
+
+    public void setFragment(PadFragment fragment) {
+        this.fragment = fragment;
+    }
+
+    public LineGraphSeries<DataPoint> getDataPlot() {
+        return dataPlot;
+    }
+
+    public void setDataPlot(LineGraphSeries<DataPoint> dataPlot) {
+        this.dataPlot = dataPlot;
+    }
+
+    public double tens(double percentOfPeriodComplete) {
+        if (!enabled){
+            return 0;
+        }
+        double Vh = getAmplitude();
+        double Vl = -Vh/3;
+
+        if (percentOfPeriodComplete < getDutyCycle()){
+            return 0.25 * Vh / Math.pow(10, percentOfPeriodComplete) + 0.75 * Vh;
+        }
+        else if (percentOfPeriodComplete < 2* getDutyCycle()){
+            return Vl;
+        }
+        else {
+            return Vl / Math.pow(4, percentOfPeriodComplete-2* getDutyCycle());
+        }
+
+    }
+
+    public double getTensValue(double time) {
+        double period = 1.0/((double) getFrequency());
+        double step = (time) % period;
+
+        double val = tens(100.0 * step / period);
+
+        return val;
+
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
 }
+
